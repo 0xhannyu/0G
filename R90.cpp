23dbd118127c250633ed23318c7089baa88db2bf
Hanu <99120539+0xhannyu@users.noreply.github.com>
2024-10-09 16:31:34 +0530

    Update R90.cpp

--- R90.cpp
+++ R90.cpp
@@ -1,4 +1,4 @@
 #include<iostream>
 #include<algorithm>
 #define Q(R,TR,C)(R+C*TR)
-using namespace std;int X=64,Y=32,S=Y*X,g[64*32];int main(){generate_n(g,S,[]{return 0;});g[S>>1]=1;for(int i=0;i<S-1;i++){int a=i-1<0?--S:i-1,b=i+1>S-1?0:i+1;g[Q(i/X+1,Y,i%X)]=g[Q(a/X,Y,a%X)]^g[Q(b/X,Y,b%X)];}for(int i=0;i<S;i++)i%X==0?cout<<'\n':cout<<g[i];;return 0;}
+using namespace std;int X=64,Y=32,S=Y*X,g[64*32];int main(){generate_n(g,S,[]{return 0;});g[S>>1]=1;for(int i=0;i<S-1;i++){int a=i-1<0?--S:i-1,b=i+1>S-1?0:i+1;g[Q(i/X+1,Y,i%X)]=g[Q(a/X,Y,a%X)]^g[Q(b/X,Y,b%X)];}for(int i=0;i<S;i++)putchar(i%X==0?'\n':g[i]);;return 0;}
